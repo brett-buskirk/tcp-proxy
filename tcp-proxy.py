941db39d095e9a3b03c644702ad1afa300e988e4
brett-buskirk <buskirkbrett8@gmail.com>
2022-08-12 22:25:54 -0400

    Added main function

--- tcp-proxy.py
+++ tcp-proxy.py
@@ -105,5 +105,29 @@ def server_loop(local_host, local_port, remote_host, remote_port, receive_first)
         # start a thread to talk to the remote host
         proxy_thread = threading.Thread(
             target=proxy_handler,
-            args=(client_socket, remote_host, remote_port, received_first))
+            args=(client_socket, remote_host, remote_port, receive_first))
         proxy_thread.start()
+
+def main():
+    if len(sys.argv[1:]) != 5:
+        print("Usage: ./tcp-proxy.py [localhost] [localport]", end='')
+        print("[remotehost [remoteport] [receive_first]")
+        print("Example: ./tcp-proxy.py 127.0.0.1 9000 10.12.132.1 9000 True")
+        sys.exit(0)
+    local_host = sys.argv[1]
+    local_port = int(sys.argv[2])
+    remote_host = sys.argv[3]
+    remote_port = int(sys.argv[4])
+
+    receive_first = sys.argv[5]
+
+    if "True" in receive_first:
+        receive_first = True
+    else:
+        receive_first = False
+
+    server_loop(local_host, local_port, remote_host, remote_port, receive_first)
+
+if __name__ == '__main__':
+    main()
+
