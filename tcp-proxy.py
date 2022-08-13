c8d12801e1bf9b04b438e8cdbf797e13a7ea4ae9
brett-buskirk <buskirkbrett8@gmail.com>
2022-08-12 22:18:23 -0400

    Added server_loop function

--- tcp-proxy.py
+++ tcp-proxy.py
@@ -83,3 +83,27 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first):
             remote_socket.close()
             print("[*] No more data. Closing connections.")
             break
+
+def server_loop(local_host, local_port, remote_host, remote_port, receive_first):
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        server.bind((local_host, local_port))
+    except Exception as e:
+        print('problem on bind: %r' % e)
+
+        print("[!!] Failed to listen on %s:%d" % (local_host, local_port))
+        print("[!!] Check for other listening sockets or correct permissions.")
+        sys.exit(0)
+
+    print("[*] Listening on %s:%d" % (local_host, local_port))
+    server.listen(5)
+    while True:
+        client_socket, addr = server.accept()
+        # print out the local connection information
+        line = "> Received incoming connection from %s:%d" % (addr[0], addr[1])
+        print(line)
+        # start a thread to talk to the remote host
+        proxy_thread = threading.Thread(
+            target=proxy_handler,
+            args=(client_socket, remote_host, remote_port, received_first))
+        proxy_thread.start()
